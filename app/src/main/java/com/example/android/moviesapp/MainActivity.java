090ac0187faceb9d9b8fa34eb53aa5fd02234624
Vicuko <victorc.fernandez@gmail.com>
2018-07-25 19:46:02 -0400

    Create menu element

--- app/src/main/java/com/example/android/moviesapp/MainActivity.java
+++ app/src/main/java/com/example/android/moviesapp/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.android.moviesapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.movies_recycler_view);
     }
 }
---
+++ app/src/main/java/com/example/android/moviesapp/utilities/NetworkUtils.java
@@ -0,0 +1,7 @@
+package com.example.android.moviesapp.utilities;
+
+/**
+ * Created by Vicuko on 25/7/18.
+ */
+public class NetworkUtils {
+}
