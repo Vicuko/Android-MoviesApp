7cba0a04a44e22b4633655bdde7b3f63c6d9a434
Vicuko <victorc.fernandez@gmail.com>
2018-07-26 16:46:18 -0400

    Load menu on create and create logic for when menu is clicked to carry out an intent to the settings activity

--- app/src/main/java/com/example/android/moviesapp/MainActivity.java
+++ app/src/main/java/com/example/android/moviesapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.android.moviesapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +17,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mRecyclerView = (RecyclerView) findViewById(R.id.movies_recycler_view);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_preferences){
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
