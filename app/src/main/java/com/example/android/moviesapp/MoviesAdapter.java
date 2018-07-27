1931d578d90dc9c8681dc6b97d53448de2377833
Vicuko <victorc.fernandez@gmail.com>
2018-07-27 19:21:04 -0400

    Inform adapter for recyclerview including data set for new searches

--- app/src/main/java/com/example/android/moviesapp/MoviesAdapter.java
+++ app/src/main/java/com/example/android/moviesapp/MoviesAdapter.java
@@ -1,34 +1,75 @@
 package com.example.android.moviesapp;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 /**
  * Created by Vicuko on 27/7/18.
  */
 public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MoviesAdapterViewHolder> {
 
+    private String[] mMoviesData;
+
+    private final MoviesAdapterOnClickHandler mClickHandler;
+
+    public interface MoviesAdapterOnClickHandler {
+        void onClick(String dataForDetail);
+    }
+
+    public MoviesAdapter(MoviesAdapterOnClickHandler clickHandler) {
+        mClickHandler = clickHandler;
+    }
+
+    public class MoviesAdapterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public final ImageView mMovieImageView;
+
+        public MoviesAdapterViewHolder(View itemView) {
+            super(itemView);
+            mMovieImageView = (ImageView) itemView.findViewById(R.id.movie_poster);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            int adapterPosition = getAdapterPosition();
+            String dataForDetail = mMoviesData[adapterPosition];
+            mClickHandler.onClick(dataForDetail);
+        }
+    }
+
     @NonNull
     @Override
-    public MoviesAdapterViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public MoviesAdapterViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        Context context = viewGroup.getContext();
+        int layoutIdForListItem = R.layout.movies_list_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(layoutIdForListItem, viewGroup, shouldAttachToParentImmediately);
+        return new MoviesAdapterViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MoviesAdapterViewHolder holder, int position) {
-
+        String posterForThisMovie = mMoviesData[position];
+        Picasso.get().load(posterForThisMovie).into(holder.mMovieImageView);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        if (null == mMoviesData) return 0;
+        return mMoviesData.length;
     }
 
-    public class MoviesAdapterViewHolder extends RecyclerView.ViewHolder {
-        public MoviesAdapterViewHolder(View itemView) {
-            super(itemView);
-        }
+    public void setMoviesData(String[] moviesData) {
+        mMoviesData = moviesData;
+        notifyDataSetChanged();
     }
 }
