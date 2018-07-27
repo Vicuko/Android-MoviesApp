de2f8a6cec55eba87916cc3b23b1de0085818f52
Vicuko <victorc.fernandez@gmail.com>
2018-07-27 18:22:02 -0400

    Create MoviesAdapter for RecycleView

--- app/src/main/java/com/example/android/moviesapp/MoviesAdapter.java
+++ app/src/main/java/com/example/android/moviesapp/MoviesAdapter.java
@@ -1,7 +1,34 @@
 package com.example.android.moviesapp;
 
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by Vicuko on 27/7/18.
  */
-public class MoviesAdapter {
+public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MoviesAdapterViewHolder> {
+
+    @NonNull
+    @Override
+    public MoviesAdapterViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MoviesAdapterViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class MoviesAdapterViewHolder extends RecyclerView.ViewHolder {
+        public MoviesAdapterViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
 }
