eb28d5e742c250d8ec3695b5d5a5a3276b7f20f8
Vicuko <victorc.fernandez@gmail.com>
2019-01-31 12:49:00 +0100

    Implement database class

--- app/src/main/java/com/example/android/moviesapp/database/AppDatabase.java
+++ app/src/main/java/com/example/android/moviesapp/database/AppDatabase.java
@@ -1,7 +1,36 @@
 package com.example.android.moviesapp.database;
 
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+import android.util.Log;
+
 /**
  * Created by Vicuko on 31/1/19.
  */
-public class MovieDatabase {
+@Database(version = 1, entities = {MovieEntry.class}, exportSchema = false)
+public abstract class AppDatabase extends RoomDatabase {
+
+    private static final String LOG_TAG = AppDatabase.class.getSimpleName();
+    private static final Object LOCK = new Object();
+    private static final String DATABASE_NAME = "movies";
+    private static AppDatabase sInstance;
+
+    public static AppDatabase getInstance(Context context) {
+        if (sInstance == null) {
+            synchronized (LOCK) {
+                Log.d(LOG_TAG, "Creating new database instance");
+                sInstance = Room.databaseBuilder(context.getApplicationContext(),
+                        AppDatabase.class, AppDatabase.DATABASE_NAME)
+                        .build();
+            }
+        }
+        Log.d(LOG_TAG, "Getting the database instance");
+        return sInstance;
+    }
+
+    // movieDao is a class annotated with @Dao.
+    public abstract MovieDao movieDao();
 }
+
--- app/src/main/java/com/example/android/moviesapp/database/MovieEntry.java
+++ app/src/main/java/com/example/android/moviesapp/database/MovieEntry.java
@@ -4,4 +4,5 @@ package com.example.android.moviesapp.database;
  * Created by Vicuko on 31/1/19.
  */
 public class MovieEntry {
+
 }
