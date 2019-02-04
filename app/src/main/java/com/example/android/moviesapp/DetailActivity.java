0bae4167dfc8413972b8ad413d1c71ce8f250fb8
Vicuko <victorc.fernandez@gmail.com>
2019-02-04 12:41:26 +0100

    Adjust layout visibility to avoid big movements on screen loading

--- app/src/main/java/com/example/android/moviesapp/DetailActivity.java
+++ app/src/main/java/com/example/android/moviesapp/DetailActivity.java
@@ -265,6 +265,8 @@ public class DetailActivity extends AppCompatActivity {
                     }
                 } else if (mConfigurationHasChanged) {
                     mTrailersAdapter.setMoviesData(videoArray);
+                } else {
+                    hideTrailerBlock();
                 }
             }
 
